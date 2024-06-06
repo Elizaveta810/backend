5a070ed9d108a8be9b74319e5d64cb60c19d75f8
Elizaveta810 <elizaveta.k810@mail.ru>
2024-06-06 13:17:41 +0300

    поправки

--- src/index.js
+++ src/index.js
@@ -3,53 +3,52 @@ const getUsers = require("./modules/users");
 const { URL } = require("url");
 
 const server = http.createServer((request, response) => {
-    const ipAddress = "http://127.0.0.1";
-    const url = new URL(request.url, ipAddress);
-    const helloValue = url.searchParams.get("hello");
+  const ipAddress = "http://127.0.0.1";
+  const url = new URL(request.url, ipAddress);
+  const helloValue = url.searchParams.get("hello");
 
-    if (helloValue) {
-        response.status = 200;
-        response.statusMessage = "Ok";
-        response.header = "Content-Type: text/plain";
-        response.write(`hello, ${helloValue}`);
-        response.end();
-        return
-      }
+  if (helloValue) {
+    response.statusCode = 200;
+    response.statusMessage = "Ok";
+    response.header = "Content-Type: text/plain";
+    response.write(`hello, ${helloValue}`);
+    response.end();
+    return;
+  }
 
-      if (request.url === "/users") {
-        response.status = 200;
-        response.statusMessage = "Ok";
-        response.header = "Content-Type: aplication/json";
-        response.write(getUsers());
-        response.end();
-    
-        return;
-      }
+  if (request.url === "/users") {
+    response.statusCode = 200;
+    response.statusMessage = "Ok";
+    response.header = "Content-Type: aplication/json";
+    response.write(getUsers());
+    response.end();
 
-      if (request.url === "/?hello") {
-        response.statusCode = 400;
-        response.statusMessage = "Bad Request";
-        response.setHeader("Content-Type", "text/plain");
-        response.write(`Enter a name`);
-        response.end();
-        return;
-      }
-    
-      if (request.url === "/") {
-        response.status = 200;
-        response.statusMessage = "Ok";
-        response.header = "Content-Type: text/plain";
-        response.write("Hello, world");
-        response.end();
-        return;
-      }
-    
-      response.status = 500;
-      response.statusMessage = "Ok";
-      response.header = "Content-Type: text/plain";
-      response.write("{}");
-      response.end();
+    return;
+  }
 
+  if (request.url === "/hello") {
+    response.statusCode = 400;
+    response.statusMessage = "Bad Request";
+    response.setHeader("Content-Type", "text/plain");
+    response.write(`Enter a name`);
+    response.end();
+    return;
+  }
+
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.statusMessage = "Ok";
+    response.header = "Content-Type: text/plain";
+    response.write("Hello, world");
+    response.end();
+    return;
+  }
+
+  response.statusCode = 500;
+  response.statusMessage = "Ok";
+  response.header = "Content-Type: text/plain";
+  response.write("{}");
+  response.end();
 
   // Написать обработчик запроса:
   // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
